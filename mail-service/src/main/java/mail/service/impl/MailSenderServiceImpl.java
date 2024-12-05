551d99eae61c1cb5b71b6e06c746afaf8ae47740
Andrei Chernetskii <ravensguard@gmail.com>
2024-12-05 14:57:25 +0100

    verify mail sending

---
+++ mail-service/src/main/java/mail/service/impl/MailSenderServiceImpl.java
@@ -0,0 +1,43 @@
+package mail.service.impl;
+
+import mail.dto.MailParams;
+import mail.service.MailSenderService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailSenderServiceImpl implements MailSenderService {
+    private final JavaMailSender javaMailSender;
+    @Value( "${spring.mail.username}" )
+    private String emailFrom;
+    @Value( "${service.activation.uri}" )
+    private String activationServiceUri;
+
+    public MailSenderServiceImpl( JavaMailSender javaMailSender ) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    @Override
+    public void send( MailParams mailParams ) {
+        String subject = "Account activation";
+        String messageBody = getActivationMailBody( mailParams.getId() );
+        String emailTo = mailParams.getEmailTo();
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom( emailFrom );
+        mailMessage.setTo( emailTo );
+        mailMessage.setSubject( subject );
+        mailMessage.setText( messageBody );
+
+        javaMailSender.send( mailMessage );
+    }
+
+    private String getActivationMailBody( String id ) {
+        String msg = String.format(
+                "To verify your account, please follow the link:\n%s ", activationServiceUri
+        );
+        return msg.replace( "{id}", id );
+    }
+}
