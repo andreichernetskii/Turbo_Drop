f579c27bfee1a84e99fcbd6081af246c13d3599c
Andrei Chernetskii <ravensguard@gmail.com>
2024-12-14 22:17:52 +0100

    Added webhook functionality for faster work

--- dispatcher/src/main/java/dispatcher/service/impl/AnswerConsumerImpl.java
+++ dispatcher/src/main/java/dispatcher/service/impl/AnswerConsumerImpl.java
@@ -1,6 +1,6 @@
 package dispatcher.service.impl;
 
-import dispatcher.controller.UpdateController;
+import dispatcher.controller.UpdateProcessor;
 import dispatcher.service.AnswerConsumer;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
@@ -10,15 +10,15 @@ import static model.RabbitQueue.ANSWER_MESSAGE_UPDATE;
 
 @Service
 public class AnswerConsumerImpl implements AnswerConsumer {
-    private final UpdateController updateController;
+    private final UpdateProcessor updateProcessor;
 
-    public AnswerConsumerImpl( UpdateController updateController ) {
-        this.updateController = updateController;
+    public AnswerConsumerImpl( UpdateProcessor updateProcessor ) {
+        this.updateProcessor = updateProcessor;
     }
 
     @Override
     @RabbitListener( queues = ANSWER_MESSAGE_UPDATE )
     public void consume( SendMessage sendMessage ) {
-        updateController.setView( sendMessage );
+        updateProcessor.setView( sendMessage );
     }
 }
