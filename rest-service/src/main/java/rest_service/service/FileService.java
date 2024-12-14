26684a1f0030bf37c0bd5e6f1351729f874ba30e
Andrei Chernetskii <ravensguard@gmail.com>
2024-12-14 20:34:58 +0100

    Refactored file sending to use ServletOutputStream with try-with-resources for safer resource management

--- rest-service/src/main/java/rest_service/service/FileService.java
+++ rest-service/src/main/java/rest_service/service/FileService.java
@@ -2,13 +2,10 @@ package rest_service.service;
 
 import common_jpa.entity.AppDocument;
 import common_jpa.entity.AppPhoto;
-import common_jpa.entity.BinaryContent;
-import org.springframework.core.io.FileSystemResource;
 
 public interface FileService {
     AppDocument getDocument( String dockId );
 
     AppPhoto getPhoto( String photoId );
 
-    FileSystemResource getFileSystemResource( BinaryContent binaryContent );
 }
--- rest-service/src/main/java/rest_service/service/impl/FileServiceImpl.java
+++ rest-service/src/main/java/rest_service/service/impl/FileServiceImpl.java
@@ -4,16 +4,10 @@ import common_jpa.dao.AppDocumentDAO;
 import common_jpa.dao.AppPhotoDAO;
 import common_jpa.entity.AppDocument;
 import common_jpa.entity.AppPhoto;
-import common_jpa.entity.BinaryContent;
 import lombok.extern.log4j.Log4j;
-import org.apache.commons.io.FileUtils;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Service;
 import rest_service.service.FileService;
 import utils.CryptoTool;
-
-import java.io.File;
-import java.io.IOException;
 
 @Log4j
 @Service
@@ -39,20 +33,5 @@ public class FileServiceImpl implements FileService {
     public AppPhoto getPhoto( String hash ) {
         Long id = cryptoTool.idOf( hash );
         return ( id != null ) ? appPhotoDAO.findById( id ).orElse( null ) : null;
-    }
-
-    @Override
-    public FileSystemResource getFileSystemResource( BinaryContent binaryContent ) {
-        try {
-            //todo: add name generation of temp file
-            File tempFile = File.createTempFile( "tempFile", ".bin" );
-            tempFile.deleteOnExit();
-            FileUtils.writeByteArrayToFile( tempFile, binaryContent.getFileAsArrayOfBytes() );
-
-            return new FileSystemResource( tempFile );
-        } catch ( IOException e ) {
-            log.error( e );
-            return null;
-        }
     }
 }
