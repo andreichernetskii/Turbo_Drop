5e8f08a8a12af1014cee505c38953ffd49925bef
Andrei Chernetskii <ravensguard@gmail.com>
2024-12-15 10:53:39 +0100

    common-jpa refacto

--- common-jpa/src/main/java/common_jpa/entity/AppPhoto.java
+++ common-jpa/src/main/java/common_jpa/entity/AppPhoto.java
@@ -1,8 +1,17 @@
 package common_jpa.entity;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Getter
 @Setter
@@ -12,11 +21,15 @@ import javax.persistence.*;
 @AllArgsConstructor
 @Entity
 public class AppPhoto {
+
     @Id
     @GeneratedValue( strategy = GenerationType.IDENTITY )
     private Long id;
+
     private String telegramFileId;
+
     @OneToOne
     private BinaryContent binaryContent;
+
     private Integer fileSize;
 }
--- common-jpa/src/main/java/common_jpa/entity/AppUser.java
+++ common-jpa/src/main/java/common_jpa/entity/AppUser.java
@@ -1,10 +1,21 @@
 package common_jpa.entity;
 
 import common_jpa.entity.enums.UserState;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 
 @Getter
@@ -16,17 +27,26 @@ import java.time.LocalDateTime;
 @Table( name = "app_user" )
 @Entity
 public class AppUser {
+
     @Id
     @GeneratedValue( strategy = GenerationType.IDENTITY )
     private Long id;
+
     private Long telegramUserId;
+
     @CreationTimestamp
     private LocalDateTime firstLoginData;
+
     private String firstName;
+
     private String lastName;
+
     private String userName;
+
     private String email;
+
     private Boolean isActive;
+
     @Enumerated( EnumType.STRING )
     private UserState state;
 }
